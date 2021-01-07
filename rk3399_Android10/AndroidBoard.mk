6199dac30f45bbe5e22ab8e10d07a04e47efb4a4
andy3_kuo <andy3_kuo@asus.com>
2021-01-07 10:33:37 +0800

    Fix sd boot fail at emmc sku device(3/3)
    Don't rebuild fstab
    
    Change-Id: I6140be64cee00fe71e63d4911ee9c9d361e73efb

--- rk3399_Android10/AndroidBoard.mk
+++ rk3399_Android10/AndroidBoard.mk
@@ -1,5 +1,5 @@
 # generate fstab file for device
--include device/rockchip/common/build/rockchip/RebuildFstab.mk
+#-include device/rockchip/common/build/rockchip/RebuildFstab.mk
 
 # generate dtbo image for device
 -include device/rockchip/common/build/rockchip/RebuildDtboImg.mk
